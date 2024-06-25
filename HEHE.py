be0f75dd001478326fcecbf94984aa105e9d7ae7
Jed <basasjed@gmail.com>
2024-06-24 23:48:24 -0700

    Update HEHE.py

--- HEHE.py
+++ HEHE.py
@@ -1,21 +1,26 @@
 
 rows = int(input("Enter the number of rows: "))
 cols = int(input("Enter the number of columns: "))
+cols = 4  
 
 
-cols = 4
+i = 0
 
 
-for i in range(rows):
-    for j in range(cols):
+while i < rows:
+   
+    j = 0
+    
+    while j < cols:
         if j == 0 or j == cols - 1:  
             print("* ", end=" ")
         elif i == 0 or i == rows - 1:  
             print("* ", end="  ")
         else:  
             print("  ", end="  ")
+        j += 1  
     print()  
-
+    i += 1  
 
 
 
